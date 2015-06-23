29a3e23bd425f0626952b854bd0ab09fe2d658b6
Anton Amirian <antonam@matrix.co.il>
2015-06-23 22:47:05 +0300

    initial init.el

---
+++ init.el
@@ -0,0 +1,69 @@
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages")
+			 ("melpa" . "http://melpa.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")))
+
+(if (equal system-type 'windows-nt)
+    (progn (setq explicit-shell-file-name
+                 "C:/Program Files (x86)/Git/bin/sh.exe")
+           (setq shell-file-name explicit-shell-file-name)
+           (setq explicit-sh.exe-args '("--login" "-i"))
+           (setenv "SHELL" shell-file-name)
+           (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)))
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq inhibit-startup-message t)
+
+(set-face-attribute 'default nil :height 90)
+
+(progn
+  (ido-mode t)
+  (setq ido-enable-flex-matching t)
+
+  (global-set-key (kbd "M-/") 'hippie-expand)
+  (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+  (show-paren-mode 1)
+  (setq-default indent-tabs-mode nil)
+
+  (setq x-select-enable-clipboard t
+        x-select-enable-primary t
+        save-interprogram-paste-before-kill t
+        apropos-do-all t
+        mouse-yank-at-point t
+        require-final-newline t
+        visible-bell t
+        load-prefer-newer t
+        ediff-window-setup-function 'ediff-setup-windows-plain
+        save-place-file (concat user-emacs-directory "places")
+        backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                                 "backups")))))
+(defun my-zenburn-init ()
+  (load-theme 'zenburn))
+
+(setq projectile-require-project-root nil
+      projectile-enable-caching t)
+
+(add-hook 'after-init-hook 'my-zenburn-init)
+(add-hook 'after-init-hook 'projectile-global-mode)
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'csharp-mode-hook 'omnisharp-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("3dafeadb813a33031848dfebfa0928e37e7a3c18efefa10f3e9f48d1993598d3" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
