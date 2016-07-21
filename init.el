2fcacdc6c4d80eb6c569cd4ffab2161cae66a895
Anton Amirian <Anton Amirian>
2016-07-21 09:50:23 +0300

    added more typescript stuff

--- init.el
+++ init.el
@@ -49,6 +49,10 @@
         tern
         company-tern
 
+        ;; typescript
+        tide
+        tss
+
         ;; Utils
         company
         anzu
@@ -173,6 +177,41 @@
   (setq web-mode-content-alist
         '(("xml" . "*\\.config\\'"))))
   
+;; Typescript stuff
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; format options
+(setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
+
+(require 'tss)
+
+;; Key binding
+;(setq tss-popup-help-key "C-:")
+;(setq tss-jump-to-definition-key "C->")
+;(setq tss-implement-definition-key "C-c i")
+
+;; Make config suit for you. About the config item, eval the following sexp.
+;; (customize-group "tss")
+
+;; Do setting recommemded configuration
+(tss-config-default)
 
 (defun my-after-init-hook ()
   (my-web-mode)
